d264ac258f40b961fb4f0dac297b0c2dd42655d7
Dee-Abby <habeebulaholayinka@gmail.com>
2024-09-07 20:14:00 +0100

    __

--- library/models.py
+++ library/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from main.models.models import School
@@ -24,7 +25,7 @@ class LibraryBook(models.Model):
     #     Library, on_delete=models.CASCADE, related_name="library")
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
-    isbn = models.CharField(max_length=20)
+    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='books' )
     book_image_url = models.URLField()
     available_copies = models.IntegerField()
 
