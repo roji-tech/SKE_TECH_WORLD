9d9fe63e00d509d52e1a0eb741c8710fcb7e934d
Dee-Abby <habeebulaholayinka@gmail.com>
2024-10-05 22:43:03 +0100

    __

---
+++ main/notification_handler.py
@@ -0,0 +1,54 @@
+from notification.models import Notification
+# class Actions:
+#     TEACHER_ADDED = "A new teacher has been added."
+#     TEACHER_UPDATED = "Teacher details have been updated."
+#     STUDENT_ADDED = "A new student has been added."
+#     STUDENT_UPDATED = "Student details have been updated."
+#     GMEET_CLASS_CREATED = "A new Google Meet class has been created."
+#     GMEET_CLASS_UPDATED = "Google Meet class details have been updated."
+#     NOTE_ADDED = "A new class note has been added."
+#     NOTE_UPDATED = "Class note has been updated."
+#     NOTE_DELETED = "Classnote has b"
+
+#     '''LESSON PLAN NOTIFICATIONS'''
+#     LESSON_PLAN_ADDED = "Class note has been updated."
+#     LESSON_PLAN_UPDATED = "Class note has been updated."
+#     LESSON_PLAN_DELETED = "Class note has been updated."
+
+class NotificationManager:
+  
+  @staticmethod
+  def create_notification(user, action, object_instance):
+    # user = request.user
+    title, message, icon = NotificationManager.get_notification_content(action, object_instance) 
+    Notification.create(user=user, title=title, message=message, icon=icon)
+
+    @staticmethod
+    def get_notification_content(action, object_instance):
+      if action == 'added':
+        title = f"New {object_instance.__class__.__name__} added"
+        message = f"{object_instance} has been added."
+        icon = "add_circle"
+
+      elif action == "updated":
+        title = f"{object_instance.__class__.__name__} updated"
+        message = f"{object_instance} has been updated."
+        icon = "edit"
+
+      elif action == "deleted":
+        title = f"{object_instance.__class__.__name__} deleted"
+        message = f"{object_instance} has been deleted."
+        icon = "delete"
+
+      else:
+        title = f"{object_instance.__class__.__name__} action performed "
+        message = f"{object_instance} has undergone {action}."
+        icon = "info"
+
+    return title, message, icon
+
+
+
+
+
+
--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+
+from main.models.models import School
 
 # Create your models here.
+
+class Notification(models.Model):
+      title = models.CharField(max_length=255)
+      message = models.TextField()
+      icon = models.CharField(max_length=50)  # Optional field for icons like 'person_add', 'assessment', etc.
+      created_at = models.DateTimeField(default=timezone.now)
+
+      def __str__(self):
+       return f"{self.title} - {self.created_at}"
+
+
+      @classmethod
+      def get_school_events(cls, request):
+          user = request.user
+          school = School.get_user_school(user)
+          """Return all events for a specific school"""
+          return cls.objects.filter(school=school)
