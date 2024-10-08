786a86351e2e2c8bcf7f49fa7bbd04b8aec8919d
roji-tech <rojitech9@gmail.com>
2024-10-08 06:58:23 +0100

    +report+

--- report/views.py
+++ report/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render, redirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -31,4 +30,32 @@ def submit_complaint(request):
             return redirect('complaints')
     else:
         form = ComplaintForm()
-    return render(request, 'report/submit_complaint.html', {'form': form})
+
+    complaints = Complaint.objects.all()
+    return render(request, 'report/submit_complaint.html', {'form': form, "complaints": complaints})
+
+
+@mydecorators.admin_is_authenticated
+def update_complaint(request, pk):
+    print(type(pk))
+    instance = get_object_or_404(Complaint, pk=pk)
+
+    form = ComplaintForm(instance=instance)
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            complaint = form.save(commit=False)
+            print(complaint.user)
+            print(complaint.user_id)
+            print(dir(complaint))
+
+            if not complaint.instance.user:
+                complaint.instance.user = request.user  # Associate complaint with the logged-in admin
+
+            complaint.save()
+            return redirect('complaints')
+    else:
+        form = ComplaintForm(instance=instance)
+
+    complaints = Complaint.objects.all()
+    return render(request, 'report/submit_complaint.html', {'form': form, "complaints": complaints})
